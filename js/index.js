41c1fc0e23ca466570b85ab71df8debbc96007a7
JoJo Myles <joneshiamyles31@gmail.com>
2021-01-13 20:07:39 -0500

    added files

---
+++ js/index.js
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bootstrap</title>
+    <link rel="stylesheet" href="css/index.css" />
+    </head>
+
+<body>
+<main >
+   <div id="contact" class="wrapper">
+       <h1>CONTACT US</h1>
+       <form id="placeholder" action="#" method="GET">
+       <div class="textEmail">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+
+
+       </form>
+
+   </div>
+    
+</main>
+</body>
+<script src="js/index.js"></script>
+</html>
